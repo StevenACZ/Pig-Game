ae6a3167002b910d0537a15a4559dd6ffa08f80f
Steven Coaila Zaa <scoailazaa@gmail.com>
2020-02-01 09:02:46 -0500

    Final version

--- app.js
+++ app.js
@@ -4,75 +4,10 @@ GAME RULES:
 - The game has 2 players, playing in rounds
 - In each turn, a player rolls a dice as many times as he whishes. Each result get added to his ROUND score
 - BUT, if the player rolls a 1, all his ROUND score gets lost. After that, it's the next player's turn
-- The player can choose to 'Hold', which means that his ROUND score gets added to his GLBAL score. After that, it's the next player's turn
+- The player can choose to 'Hold', which means that his ROUND score gets added to his GLOBAL score. After that, it's the next player's turn
 - The first player to reach 100 points on GLOBAL score wins the game
 
 */
-
-var scores, roundScore, activePlayer, gamePlaying;
-
-init();
-
-document.querySelector('.btn-roll').addEventListener('click', function () {
-  if (gamePlaying) {
-    // 1. Random rumber
-    var dice = Math.floor(Math.random() * 6) + 1;
-
-    // 2. Display the result
-    var diceDOM = document.querySelector('.dice');
-    diceDOM.src = 'dice-' + dice + '.png';
-    diceDOM.style.display = 'block';
-    
-    // 3. Update the round score IF the rolled number was NOT a 1
-    if (dice !== 1) {
-      // Add score
-      roundScore += dice;
-      document.getElementById('current-' + activePlayer).textContent = roundScore;
-    } else {
-      // Next score
-      nextPlayer();
-    }
-  } 
-
-});
-
-document.querySelector('.btn-hold').addEventListener('click', function () {
-  if (gamePlaying) {
-    // Add CURRENT score to GLOBAL score
-    scores[activePlayer] += roundScore;
-    document.getElementById('score-' + activePlayer).textContent = scores[activePlayer];
-  
-    // Update the UI
-  
-    // Check if player won the game
-    if (scores[activePlayer] >= 20) {
-      document.getElementById('name-' + activePlayer).textContent = 'Winner!';
-      document.querySelector('.dice').style.display = 'none';
-      document.querySelector('.player-' + activePlayer + '-panel').classList.add('winner');
-      document.querySelector('.player-' + activePlayer + '-panel').classList.remove('active');
-  
-      gamePlaying = false;
-    } else {
-      // Next player
-      nextPlayer();
-    }
-  }
-
-});
-
-function nextPlayer () {
-  activePlayer === 0 ? activePlayer = 1 : activePlayer = 0;
-  roundScore = 0;
-  document.getElementById('current-0').textContent = '0';
-  document.getElementById('current-1').textContent = '0';
-
-  document.querySelector('.player-0-panel').classList.toggle('active');
-  document.querySelector('.player-1-panel').classList.toggle('active');
-
-  document.querySelector('.dice').style.display = 'none';
-}
-
-document.querySelector('.btn-new').addEventListener('click', init);
 
 function init() {
   scores = [0, 0];
@@ -94,6 +29,77 @@ function init() {
   document.querySelector('.player-1-panel').classList.remove('active');
   document.querySelector('.player-0-panel').classList.add('active');
 }
+
+function nextPlayer() {
+  // Who is the active player?
+  activePlayer === 0 ? activePlayer = 1 : activePlayer = 0;
+
+  roundScore = 0;
+  document.getElementById('current-0').textContent = '0';
+  document.getElementById('current-1').textContent = '0';
+
+  document.querySelector('.player-0-panel').classList.toggle('active');
+  document.querySelector('.player-1-panel').classList.toggle('active');
+
+  document.querySelector('.dice').style.display = 'none';
+}
+
+var scores, roundScore, activePlayer, gamePlaying;
+
+init();
+
+// NEW GAME BUTTON
+document.querySelector('.btn-new').addEventListener('click', init);
+
+// ROLL BUTTON
+document.querySelector('.btn-roll').addEventListener('click', function() {
+  if (gamePlaying) {
+    // 1. Random rumber
+    var dice = Math.floor(Math.random() * 6) + 1;
+
+    // 2. Display the result
+    var diceDOM = document.querySelector('.dice');
+
+    diceDOM.src = 'dice-' + dice + '.png';
+    diceDOM.style.display = 'block';
+    
+    // 3. Update the round score IF the rolled number is equal to 1
+    if (dice === 1) {
+      // Next score
+      nextPlayer();
+    } else {
+      // Add score
+      roundScore += dice;
+      document.getElementById('current-' + activePlayer).textContent = roundScore;
+    }
+  } 
+});
+
+// HOLD BUTTON
+document.querySelector('.btn-hold').addEventListener('click', function () {
+  if (gamePlaying) {
+    // Add CURRENT score to GLOBAL score
+    scores[activePlayer] += roundScore;
+    document.getElementById('score-' + activePlayer).textContent = scores[activePlayer];
+  
+    // Update the UI
+  
+    // Check if player won the game
+    if (scores[activePlayer] >= 10) {
+      document.getElementById('name-' + activePlayer).textContent = 'Winner!';
+      document.querySelector('.dice').style.display = 'none';
+      document.querySelector('.player-' + activePlayer + '-panel').classList.add('winner');
+      document.querySelector('.player-' + activePlayer + '-panel').classList.remove('active');
+      
+      // End the game
+      gamePlaying = false;
+    } else {
+      // Next player
+      nextPlayer();
+    }
+  }
+
+});
 
 /*
 
